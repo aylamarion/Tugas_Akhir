f9bea5687395a3781b321799d51b74003bb5c0c0
root <root@ip-172-31-18-79.ap-southeast-1.compute.internal>
2025-06-12 15:03:10 +0000

    fix conflict

--- database.py
+++ database.py
@@ -8,9 +8,9 @@ load_dotenv()
 url_object = URL.create(
     "mysql+pymysql",
     username="root",  # os.getenv("DB_USERNAME"),
-    # password="password",  # os.getenv("DB_PASSWORD"),
+    password="adMin93@3",  # os.getenv("DB_PASSWORD"),
     host="localhost",  # os.getenv("DB_HOST"),
-    database="data jantung",  # os.getenv("DB_NAME"),
+    database="data_jantung",  # os.getenv("DB_NAME"),
     port=os.getenv("DB_PORT"),
 )
 engine = create_engine(url_object)
