1006ccb1c7002346ca9668e2b29017aa3f667d25
aylamarion <mariongabe823@gmail.com>
2025-06-13 12:15:27 +0700

    add s3 bucket

--- app.py
+++ app.py
@@ -37,12 +37,12 @@ app.teardown_appcontext(lambda exc: db_session.close())
 user_datastore = SQLAlchemySessionUserDatastore(db_session, User, Role)
 security = Security(app, user_datastore)
 
-# s3 = boto3.client('s3',
-# aws_access_key_id= os.getenv("AWS_ACCESS_KEY_ID"),
-# aws_secret_access_key= os.getenv("AWS_SECRET_ACCESS_KEY"),
-# region_name= os.getenv("AWS_REGION"))
+s3 = boto3.client('s3',
+aws_access_key_id= os.getenv("AWS_ACCESS_KEY_ID"),
+aws_secret_access_key= os.getenv("AWS_SECRET_ACCESS_KEY"),
+region_name= os.getenv("AWS_REGION"))
 
-# s3_bucket = os.getenv("AWS_BUCKET_NAME")
+s3_bucket = os.getenv("AWS_BUCKET_NAME")
 
 
 class HelloWorld(Resource):
